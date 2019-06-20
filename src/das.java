732a373e6f857a53e611f9da54aa3511b2e4e1a1
LJP1033901215 <LJP1033901215@users.noreply.github.com>
2019-06-20 10:17:20 +0800

    Update das.java

--- src/das.java
+++ src/das.java
@@ -1,6 +1,12 @@
 public class das {
     public static void main(String[] args){
-        System.out.println("这是啥啊");
+        System.out.println("这是啥啊!,这是九九乘法表");
+        for (int i=1; i<=9;i++){
+            System.out.println();
+            for (int t=1 ; t<=i;t++){
+                System.out.print(i+"*"+t+"="+(i*t)+" ");
+            }
+        }
     }
 
 }
